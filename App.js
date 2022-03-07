0f247099195000c08e455283852da1656597803c
Mobcoder ID-105 <mobcoderid-105@Mobcoders-Mac-mini-2.local>
2022-03-07 11:32:54 +0530

    read and write data from firebase

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {Button, SafeAreaView, Text} from 'react-native';
+
+import firestore from '@react-native-firebase/firestore';
 
 const App = () => {
+  const ref = firestore().collection('users');
+  const handleAddUser = () => {
+    ref
+      .add({
+        name: 'Sunil Kumar Yadav',
+        age: 24,
+      })
+      .then(() => {
+        console.log('User Added');
+      });
+  };
+
+  const handleGetUser = () => {
+    ref.onSnapshot(res => {
+      res.forEach(doc => {
+        let {name, age} = doc.data();
+        console.log(name, ' ', age, 'asdfghjkl;');
+      });
+    });
+  };
+
   return (
     <SafeAreaView>
       <Text>Hello</Text>
+      {console.log(
+        ref.get().then(res => {
+          console.log('asdfghjkl;');
+        }),
+      )}
+      <Button title="Add" onPress={handleAddUser} />
+      <Text>Get User</Text>
+      <Button title="Get" onPress={handleGetUser} />
     </SafeAreaView>
   );
 };
 
 export default App;
+
+const user = {
+  name: 'sunil',
+};
+
+const DataFormate = {
+  'Strategic EnterPrise Operation': {total: 10, avg: 6},
+  Leadership: {total: 10, avg: 6},
+  'Culture Value': {total: 10, avg: 6},
+  'Culture Beliefs': {total: 10, avg: 6},
+  'Culture Assumptions': {total: 10, avg: 6},
+  Resillience: {total: 10, avg: 6},
+  Skill: {total: 10, avg: 6},
+};
+
+const COURSE = [
+  'Strategic EnterPrise Operation',
+  'Leadership',
+  'Culture Value',
+  'Culture Beliefs',
+  'Culture Assumptions',
+  'Resillience',
+  'Skill',
+];
---
+++ src/firebase/Collection.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {firebase} from '@react-native-firebase/firestore';
+
+const Collection = () => {
+  const ref = firebase().collection('users');
+  return null;
+};
+
+export default Collection;
